95929c2603ba8d866b28226f11a7ba78de00202c
Rob Taylor <robbytaylor@users.noreply.github.com>
2020-02-02 17:42:00 +0000

    Added output for bucket name

--- output.tf
+++ output.tf
@@ -2,6 +2,10 @@ output bucket_arn {
   value = aws_s3_bucket.bucket.arn
 }
 
+output bucket_name {
+  value = aws_s3_bucket.bucket.bucket
+}
+
 output cloudfront_zone_id {
   value = aws_cloudfront_distribution.default.hosted_zone_id
 }
